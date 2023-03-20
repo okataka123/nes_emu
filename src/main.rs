87bf6dec92a273831a4fce90cac0149636d1149f
okataka123 <tekepika@gmail.com>
2023-03-21 00:20:08 +0900

    implemented and tested INX

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,10 @@ impl CPU {
         self.register_x = self.register_a;
         self.update_zero_and_negative_flags(self.register_x);
     }
+    fn inx(&mut self) {
+        self.register_x = self.register_x.wrapping_add(1);
+        self.update_zero_and_negative_flags(self.register_x);
+    }
 
     fn update_zero_and_negative_flags(&mut self, result: u8) {
         if result == 0 {
@@ -61,6 +65,9 @@ impl CPU {
                 // TAX Implied
                 0xAA => self.tax(),
 
+                // INX Implied.
+                0xE8 => self.inx(),
+
                 _ => todo!()
             }
         }
@@ -89,7 +96,7 @@ mod test {
     fn test_0xa9_lda_negative_flag() {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0x80, 0x00]);
-        assert!(cpu.status | 0b0100_0000 != 0);
+        assert!(cpu.status & 0b1000_0000 != 0);
     }
     #[test]
     fn test_0xaa_tax_move_a_to_x() {
@@ -97,5 +104,19 @@ mod test {
         cpu.register_a = 10;
         cpu.interpret(vec![0xaa, 0x00]);
         assert_eq!(cpu.register_x, 10);
+    }
+    #[test]
+    fn test_5_ops_working_together() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+        assert_eq!(cpu.register_x, 0xc1);
+    }
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = CPU::new();
+        cpu.register_x = 0xff;
+        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+        println!("cpu.register_x = {}", cpu.register_x);
+        assert_eq!(cpu.register_x, 1);
     }
 }
